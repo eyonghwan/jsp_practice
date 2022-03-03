43ae35aafb35bdc07a515158939c6150689ee52f
eyonghwan <popularisyong@gmail.com>
2022-03-03 12:15:32 +0900

    3월 3일 forward

--- MyFirstWeb/src/main/java/kr/co/ict/servlet/ServletCustom.java
+++ MyFirstWeb/src/main/java/kr/co/ict/servlet/ServletCustom.java
@@ -1,6 +1,8 @@
 package kr.co.ict.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,7 +36,7 @@ public class ServletCustom extends HttpServlet {
 	 * @see Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("/custom 접속시 생성된 객체는 서버 종료로 인해 파기됩니다.");
 	}
 
 	/**
@@ -46,19 +48,44 @@ public class ServletCustom extends HttpServlet {
 		// request.getParameter() 를 이용해
 		// "jsp", "boot" 라는 이름으로 들어온 요소를 콘솔에 찍도록 아래 코드를 작성하고
 		// 확인하는 방법은 ?jsp=값1&boot=값2 를 주소 뒤에 붙입니다.
-		System.out.println(request.getParameter("jsp"));
-		System.out.println(request.getParameter("boot"));
+		String jsp = request.getParameter("jsp");
+		String boot = request.getParameter("boot");
+		System.out.println(jsp);
+		System.out.println(boot);
 		// "jpa"라는 이름으로 들어온 요소를 콘솔에 찍도록 추가.
 		String jpa = request.getParameter("jpa");
 		System.out.println(jpa);
+		// response.sendRedirect("http://localhost:8181/MyFirstWeb/servletForm/customResult.jsp");
+		// 상단 변수를 전달하기 위해 forward를 대신 사용합니다.
+		// 포워딩 하기 전 request에 setAttribute()를 이용해 데이터를 저장합니다.
+		// request.setAttribute("보낼이름", 자료);
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		// 목적지 주소는 localhost:포트번호/프로젝트명/이후경로
+		RequestDispatcher dp = request.getRequestDispatcher(
+				"/servletForm/customResult.jsp");
+		dp.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doPost는 현재까지 form에서 post방식으로 제출될 때 호출됩니다.
+		request.setCharacterEncoding("utf-8");
+		System.out.println("/custom 주소 post방식으로 접속함");
+		String jsp = request.getParameter("jsp");
+		String boot = request.getParameter("boot");
+		String jpa = request.getParameter("jpa");
+		System.out.println("post방식 : " + jsp + ", " + boot + ", " + jpa);
+		// response.sendRedirect("http://localhost:8181/MyFirstWeb/servletForm/customResult.jsp");
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		RequestDispatcher dp = request.getRequestDispatcher(
+				"/servletForm/customPostResult.jsp");
+		dp.forward(request, response);
 	}
 
 }
